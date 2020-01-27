7023bf597d044ae87bb01c7b4434059f2abb8372
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-01-27 17:24:10 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,21 +1,25 @@
-deli_counter = []
-
-def take_a_number(katz_deli)
-
-ordered_names = katz_deli.map.each.with_index do |name, index|
- "#{index + 1}. #{name}"
-end
-puts take_a_number(katz_deli).join(" ")
-
-end
-
-def line(deli_counter) 
-  if deli_counter.count() == 0 
+katz_deli = []
+def line(katz_deli) 
+  if katz_deli.count() == 0 
     puts "The line is currently empty."
   else
-    take_a_number(katz_deli)
-    
-    
+    ordered_names = katz_deli.map.each.with_index do |name, index|
+"#{index + 1}. #{name}"
+end
+names = ordered_names.join(" ")
+puts "The line is currently: #{names}"
   end
 end
-  
+  def take_a_number(katz_deli,name)
+    katz_deli << name
+    index = katz_deli.find_index(name)
+    puts "Welcome, #{name}. You are number #{index + 1} in line."
+end
+def now_serving(katz_deli)
+  if katz_deli.count() == 0 
+    puts "There is nobody waiting to be served!"
+  else
+    katz_deli.shift
+    puts "Currently serving #{katz_deli.first}"
+  end
+end
