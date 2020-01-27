56d7eafcddda8cec898d4295e00e968b162f7b47
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-01-27 17:24:01 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,21 @@
-# Write your code here.
+deli_counter = []
+
+def take_a_number(katz_deli)
+
+ordered_names = katz_deli.map.each.with_index do |name, index|
+ "#{index + 1}. #{name}"
+end
+puts take_a_number(katz_deli).join(" ")
+
+end
+
+def line(deli_counter) 
+  if deli_counter.count() == 0 
+    puts "The line is currently empty."
+  else
+    take_a_number(katz_deli)
+    
+    
+  end
+end
+  
